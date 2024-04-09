47c4c52e7d5a032d9bec8b7ab3cbcd15c687b764
gyl <1265126315@qq.com>
2024-04-09 20:54:06 +0800

    update

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,26 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default();
+        } else {
+            let v = s.splitn(2, ",").collect::<Vec<&str>>();
+            let name = v[0].to_string();
+            if v[0] == "" || v.len() == 1 {
+                return Person::default();
+            } else {
+                return match v[1].parse::<usize>() {
+                    Ok(age) => Person {
+                        name,
+                        age
+                    },
+                    Err(_) => Person::default()
+                };
+            }
+        }
     }
 }
 
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -31,7 +31,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
 
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
@@ -52,6 +51,27 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0 {
+            return Err(ParsePersonError::Empty);
+        } else {
+            let v = s.split(",").collect::<Vec<&str>>();
+            if v.len() != 2 {
+                return Err(ParsePersonError::BadLen);
+            } else {
+                let name = v[0].to_string();
+                if name == "" {
+                    return Err(ParsePersonError::NoName);
+                }
+                let age = match v[1].parse::<usize>() {
+                    Ok(age) => age,
+                    Err(e) => return Err(ParsePersonError::ParseInt(e)),
+                };
+                return Ok(Person {
+                    name,
+                    age
+                });
+            }
+        }
     }
 }
 
